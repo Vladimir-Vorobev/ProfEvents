52e6e933433ca8eb329dd0a5992ffa9e1852e84d
SerPan23 <igracom@yandex.ru>
2020-07-07 14:22:16 +0300

    Update Portfolio.vue

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -1,7 +1,27 @@
 <template>
   <div class="main">
     <div class="container warp">
-
+        <div class="card">
+            <div class="card-body">
+                <div class="input-group">
+                    <div class="custom-file">
+                        <input type="file" class="custom-file-input" id="inputGroupFile04" aria-describedby="inputGroupFileAddon04">
+                        <label class="custom-file-label" for="inputGroupFile04" style="text-align: left">Выбрать файл</label>
+                    </div>
+                    <div class="input-group-append">
+                        <button class="btn btn-outline-success" type="button" id="inputGroupFileAddon04">Добавить</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div>
+            <div class="card" >
+                <img src="./../assets/for_main.jpg" class="card-img-top">
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>
     </div>
     <div class="footer"><Footer></Footer></div> 
   </div>
@@ -22,8 +42,7 @@ export default {
 <style scoped>
 .warp{
     flex: 1 0 auto;
-    padding-top: 210px !important;
-    background-color: #fff;
+    padding-top: 110px !important;
     padding: 0px 30px;
 }
 .footer{
@@ -40,5 +59,21 @@ export default {
     margin-bottom: 0px;
 }
 
+.custom-file-input:lang(ru)~.custom-file-label::after {
+    content: "Выбрать";
+}
 
+.card{ 
+    margin: 20px 0px !important; 
+}
+.card-body{ 
+    text-align: left !important;
+}
+.card img{
+    display: block;
+    max-width: 100%;
+    max-height: 100%;
+    min-width: 310px;
+    min-height: 310px;
+}
 </style>
