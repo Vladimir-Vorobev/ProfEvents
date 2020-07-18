3fb84852f513e5ee143958ddaea388239476a9bf
SerPan23 <igracom@yandex.ru>
2020-07-18 17:50:53 +0300

    Update CookieNotify.vue

--- src/components/CookieNotify.vue
+++ src/components/CookieNotify.vue
@@ -1,11 +1,9 @@
 <template>
-    <div>
-        <div id="cookie_notification">
-            <p>Для улучшения работы сайта и его взаимодействия с пользователями мы используем файлы cookie. Продолжая работу с
-                сайтом, Вы разрешаете использование cookie-файлов. Вы всегда можете отключить файлы cookie в настройках Вашего
-                браузера.</p>
-            <button class="btn btn-rounded-outline-red-orange cookie_accept" @click="acceptCookie()">Принять</button>
-        </div>
+    <div id="cookie_notification">
+        <p>Для улучшения работы сайта и его взаимодействия с пользователями мы используем файлы cookie. Продолжая работу с
+            сайтом, Вы разрешаете использование cookie-файлов. Вы всегда можете отключить файлы cookie в настройках Вашего
+            браузера.</p>
+        <button class="btn btn-rounded-outline-red-orange cookie_accept" @click="acceptCookie()">Принять</button>
     </div>
 </template>
 
