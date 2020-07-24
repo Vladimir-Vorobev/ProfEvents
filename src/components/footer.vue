93df19c0b509c0004ac3216a2350a5d20b56d19c
SerPan23 <igracom@yandex.ru>
2020-07-24 14:04:24 +0300

    u

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,8 +1,20 @@
 <template>
 <div>
     <footer class="mt-auto py-3">
-        <div class="container">
+        <!-- <div class="container">
             <span class="text-muted">Разработано DARK_LED</span>
+        </div> -->
+        <div class="row" style="margin: 0px">
+            <div class="col-4" style="text-align: left !important;">
+                <a href="#" class="links">Разработано DARK_LED</a>
+            </div>
+            <div class="col-3" style="text-align: center !important;">
+                <span class="text-muted">&#169; Profevents, 2020</span>
+            </div>
+            <div class="col-5" style="text-align: right !important;">
+                <a href="#" class="links">Нашли ошибку? </a>
+                <a href="#" class="links">Политика конфиденциальности</a>
+            </div>
         </div>
     </footer>
 </div>
@@ -20,5 +32,14 @@ export default {
 <style scoped>
 footer {
     background-color: #f8f9fa;
+}
+.links{
+    color: #6C757D;
+    cursor: pointer;
+    text-decoration: none;
+}
+.links:hover{
+    color: #EF5B65;
+    text-decoration: none;
 }
 </style>
