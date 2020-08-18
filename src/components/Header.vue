5a9532af35af3644f32174dfcefc7fb0167d3aae
SerPan23 <igracom@yandex.ru>
2020-08-18 22:04:45 +0300

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -148,19 +148,6 @@ export default {
     }
 
 }
-// $(document).ready(function () {
-//     //animated-bootstrap-hamburger
-//     $('.navbar-collapse').on('click', function () {
-//         $('.navbar-collapse').collapse('hide');
-//         $('.navbar-toggler').removeClass("active");
-//     });
-
-//     $(".navbar-toggler").on("click", function () {
-//         $(this).toggleClass("active");
-//     });
-//     //---------------------
-
-// });
 </script>
 
 <style scoped>
