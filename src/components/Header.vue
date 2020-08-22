7967c3965c781a4c3eb0cd1e459b3fe0fb7ca5b4
SerPan23 <igracom@yandex.ru>
2020-08-22 15:48:35 +0300

    u

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -103,7 +103,7 @@ export default {
         .then(data => {
             if(data == '310'){
               console.log('header ' + email + ' ' + SessionID)
-              alert('header ' + email + ' ' + SessionID)
+            //   alert('header ' + email + ' ' + SessionID)
               document.cookie = "email=" + ";expires=Thu, 01 Jan 1970 00:00:01 GMT"
               document.cookie = "SessionID=" + ";expires=Thu, 01 Jan 1970 00:00:01 GMT"
               window.location.reload()
