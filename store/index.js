25f23164fd5c0da5ec3141ce478b9598cc9c834d
SerPan23 <igracom@yandex.ru>
2020-08-23 17:05:04 +0300

    Update index.js

--- store/index.js
+++ store/index.js
@@ -4,8 +4,8 @@ export const APP_STORE = {
 		allEventsScroll: 0,
 		openDaysScroll: 0,
 		SessionID: 0,
-		serverIp: 'https://makual.ru',
-		socketIp: 'https://makual.ru',
+		serverIp: 'https://profeventsserver.tk',
+		socketIp: 'https://profeventsserver.tk',
 	},
 	mutations: {
 		GET_USER_EMAIL(state, value) {
